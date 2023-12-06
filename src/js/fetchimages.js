aa03d3f7032231b45fd686161dcc53184846694d
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2023-12-06 21:39:34 +0100

    t-11

--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -1,28 +1,15 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
 
-//Унікальний ключ доступу до API Pixabay
-const KEY = '41088476-3306dd45aae3fbb545cf421af';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '41088476-3306dd45aae3fbb545cf421af';
 
-//Ліміт отриманих об'ектів на сторінці
-const pageLimit = 40;
-
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-//Функція, що фетчить картинки
-const fetchImages = async (queryToFetch, pageToFetch) => {
-  const { data } = await axios({
-    params: {
-      key: KEY,
-      q: queryToFetch,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: pageLimit,
-      page: pageToFetch,
-    },
-  });
-  return data;
-};
-
-export { fetchImages, pageLimit };
+export async function fetchImages(value, page = 1, perPage = 40) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
